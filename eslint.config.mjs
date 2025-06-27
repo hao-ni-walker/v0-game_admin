d60df2d78ae11ad602ba070c792fd188f549b7a5
Guizimo <17680262548@163.com>
2025-06-27 15:03:26 +0800

    feat: 更新配置文件，优化 ESLint 规则和忽略文件
    - 在 .gitignore 中添加 .env.test 文件，确保测试环境配置不被提交。
    - 在 eslint.config.mjs 中禁用 react/no-unescaped-entities 规则，提升代码灵活性。

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
       "@next/next/no-assign-module-variable": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
+      "react/no-unescaped-entities": "off",
     },
   },
 ];
